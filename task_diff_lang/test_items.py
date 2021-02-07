2be2062c47d9341e8b4955ad4f57ccf93bbe97c1
Anna <ragebluhane@gmail.com>
2021-02-08 02:34:21 +0700

    FIX: selector and conftest: params name

--- task_diff_lang/test_items.py
+++ task_diff_lang/test_items.py
@@ -3,5 +3,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 def test_check_item_in_cart(browser):
     browser.get(link)
-    btn = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
+    btn = browser.find_element_by_css_selector("btn btn-primary btn-block")
     assert btn, "Button not found!"
