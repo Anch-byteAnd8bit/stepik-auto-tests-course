de27117e2de80456cd8be085be2c68d6091fd854
Anna <ragebluhane@gmail.com>
2021-02-08 03:22:56 +0700

    Рабочая версия!

--- task_diff_lang/conftest.py
+++ task_diff_lang/conftest.py
@@ -6,8 +6,8 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language for your web-page: ec or fr")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language for your webpage")
 
 
 @pytest.fixture(scope="function")
@@ -23,9 +23,8 @@ def browser(request):
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
 
--- task_diff_lang/test_items.py
+++ task_diff_lang/test_items.py
@@ -1,7 +1,9 @@
-link = "http://selenium1py.pythonanywhere.com/"
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_check_item_in_cart(browser):
     browser.get(link)
-    btn = browser.find_element_by_css_selector("btn btn-primary btn-block")
+    btn = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert btn, "Button not found!"
